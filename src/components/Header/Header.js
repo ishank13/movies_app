4fe1a7e9291de3c0f87fbdab696e57ffce615e25
Ishank Gupta <ishankgupta1356@gmail.com>
2021-08-28 00:32:46 +0530

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () =>{
+    return(
+        <span onClick={()=>window.scroll(0,0)} className='header'>🎥Movies App🎬</span>
+    )
+}
+
+export default Header;
